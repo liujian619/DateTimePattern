2f2525b5677620ca5c5a178956c292508335160a
刘健 <656165271@qq.com>
2016-10-19 15:54:50 +0800

    DateTimePattern
    C#时间字符串格式化工具

---
+++ DateTimePattern/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace DateTimePattern
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必需的设计器变量。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清理所有正在使用的资源。
+		/// </summary>
+		/// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows 窗体设计器生成的代码
+
+		/// <summary>
+		/// 设计器支持所需的方法 - 不要
+		/// 使用代码编辑器修改此方法的内容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.cmb = new System.Windows.Forms.ComboBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.txt = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.lbl = new System.Windows.Forms.Label();
+			this.ckb = new System.Windows.Forms.CheckBox();
+			this.ckbEnable = new System.Windows.Forms.CheckBox();
+			this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+			this.SuspendLayout();
+			// 
+			// cmb
+			// 
+			this.cmb.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cmb.FormattingEnabled = true;
+			this.cmb.IntegralHeight = false;
+			this.cmb.Location = new System.Drawing.Point(75, 17);
+			this.cmb.MaxDropDownItems = 10;
+			this.cmb.Name = "cmb";
+			this.cmb.Size = new System.Drawing.Size(171, 27);
+			this.cmb.TabIndex = 1;
+			this.cmb.SelectedIndexChanged += new System.EventHandler(this.cmb_SelectedIndexChanged);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(18, 20);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(51, 20);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "格式：";
+			// 
+			// txt
+			// 
+			this.txt.BackColor = System.Drawing.Color.Wheat;
+			this.txt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.txt.Location = new System.Drawing.Point(75, 59);
+			this.txt.Name = "txt";
+			this.txt.ReadOnly = true;
+			this.txt.Size = new System.Drawing.Size(171, 25);
+			this.txt.TabIndex = 3;
+			this.txt.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_KeyPress);
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(18, 99);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(107, 20);
+			this.label2.TabIndex = 4;
+			this.label2.Text = "当前时间显示：";
+			// 
+			// lbl
+			// 
+			this.lbl.BackColor = System.Drawing.Color.Wheat;
+			this.lbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+			this.lbl.Location = new System.Drawing.Point(75, 124);
+			this.lbl.Name = "lbl";
+			this.lbl.Size = new System.Drawing.Size(171, 109);
+			this.lbl.TabIndex = 6;
+			this.lbl.Text = "\r\n";
+			// 
+			// ckb
+			// 
+			this.ckb.AutoSize = true;
+			this.ckb.Checked = true;
+			this.ckb.CheckState = System.Windows.Forms.CheckState.Checked;
+			this.ckb.Location = new System.Drawing.Point(131, 97);
+			this.ckb.Name = "ckb";
+			this.ckb.Size = new System.Drawing.Size(84, 24);
+			this.ckb.TabIndex = 5;
+			this.ckb.Text = "本地格式";
+			this.ckb.UseVisualStyleBackColor = true;
+			this.ckb.CheckedChanged += new System.EventHandler(this.ckb_CheckedChanged);
+			// 
+			// ckbEnable
+			// 
+			this.ckbEnable.AutoSize = true;
+			this.ckbEnable.Location = new System.Drawing.Point(12, 59);
+			this.ckbEnable.Name = "ckbEnable";
+			this.ckbEnable.Size = new System.Drawing.Size(54, 24);
+			this.ckbEnable.TabIndex = 2;
+			this.ckbEnable.Text = "edit";
+			this.ckbEnable.UseVisualStyleBackColor = true;
+			this.ckbEnable.CheckedChanged += new System.EventHandler(this.ckbEnable_CheckedChanged);
+			// 
+			// toolTip
+			// 
+			this.toolTip.AutoPopDelay = 5000;
+			this.toolTip.InitialDelay = 100;
+			this.toolTip.ReshowDelay = 100;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(265, 242);
+			this.Controls.Add(this.ckbEnable);
+			this.Controls.Add(this.ckb);
+			this.Controls.Add(this.lbl);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.txt);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.cmb);
+			this.Font = new System.Drawing.Font("微软雅黑", 10F);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.MaximizeBox = false;
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "时间日期格式";
+			this.TopMost = true;
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ComboBox cmb;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox txt;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label lbl;
+		private System.Windows.Forms.CheckBox ckb;
+		private System.Windows.Forms.CheckBox ckbEnable;
+		private System.Windows.Forms.ToolTip toolTip;
+	}
+}
+
---
+++ DateTimePattern/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using System.Globalization;
+
+namespace DateTimePattern
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+			this.cmb.Items.AddRange(FORMAT_DIC.Keys.ToArray());
+		}
+
+		static Form1()
+		{
+			FORMAT_DIC.Add("d", "短日期。");
+			FORMAT_DIC.Add("%d", "月中的某一天，一位数的日期没有前导零。" + Desc("d"));
+			FORMAT_DIC.Add("dd", "月中的某一天，一位数的日期有一个前导零。");
+			FORMAT_DIC.Add("ddd", "周中某天的缩写名称，在AbbreviatedDayNames中定义。");
+			FORMAT_DIC.Add("dddd", "周中某天的完整名称，在DayNames中定义。");
+			FORMAT_DIC.Add("D", "长日期。");
+			FORMAT_DIC.Add("f", "长日期和短时间。");
+			FORMAT_DIC.Add("%f", "毫秒的小数精度为1位，其余数字被截断。" + Desc("f"));
+			FORMAT_DIC.Add("ff", "毫秒的小数精度为2位，其余数字被截断。");
+			FORMAT_DIC.Add("fff", "毫秒的小数精度为3位，其余数字被截断。");
+			FORMAT_DIC.Add("ffff", "毫秒的小数精度为4位，其余数字被截断。");
+			FORMAT_DIC.Add("fffff", "毫秒的小数精度为5位，其余数字被截断。");
+			FORMAT_DIC.Add("ffffff", "毫秒的小数精度为6位，其余数字被截断。");
+			FORMAT_DIC.Add("fffffff", "毫秒的小数精度为7位，其余数字被截断。");
+			FORMAT_DIC.Add("F", "长日期和长时间。");
+			FORMAT_DIC.Add("g", "短日期和短时间。");
+			FORMAT_DIC.Add("gg", "时期或纪元，如果要设置格式的日期不具有关联的时期或纪元字符串，则忽略该模式。");
+			FORMAT_DIC.Add("G", "短日期和长时间。");
+			FORMAT_DIC.Add("%h", "12 小时制的小时，一位数的小时数没有前导零。");
+			FORMAT_DIC.Add("hh", "12 小时制的小时，一位数的小时数有前导零。");
+			FORMAT_DIC.Add("%H", "24 小时制的小时，一位数的小时数没有前导零。");
+			FORMAT_DIC.Add("HH", "24 小时制的小时，一位数的小时数有前导零。");
+			FORMAT_DIC.Add("m", "月和日。");
+			FORMAT_DIC.Add("%m", "分钟，一位数的分钟数没有前导零。" + Desc("m"));
+			FORMAT_DIC.Add("mm", "分钟，一位数的分钟数有一个前导零。");
+			FORMAT_DIC.Add("M", "月和日。");
+			FORMAT_DIC.Add("%M", "月份数字，一位数的月份没有前导零。" + Desc("M"));
+			FORMAT_DIC.Add("MM", "月份数字，一位数的月份有一个前导零。");
+			FORMAT_DIC.Add("MMM", "月份的缩写名称，在AbbreviatedMonthNames中定义。");
+			FORMAT_DIC.Add("MMMM", "月份的完整名称，在MonthNames中定义。");
+			FORMAT_DIC.Add("r", "RFC1123Pattern");
+			FORMAT_DIC.Add("R", "RFC1123Pattern");
+			FORMAT_DIC.Add("s", "使用当地时间的 SortableDateTimePattern（基于ISO8601）");
+			FORMAT_DIC.Add("%s", "秒，一位数的秒数没有前导零。");
+			FORMAT_DIC.Add("ss", "秒，一位数的秒数有一个前导零。");
+			FORMAT_DIC.Add("t", "短时间。");
+			FORMAT_DIC.Add("%t", "在AMDesignator或PMDesignator中定义的AM/PM指示项的第一个字符（如果存在）。" + Desc("t"));
+			FORMAT_DIC.Add("tt", "在AMDesignator或PMDesignator中定义的AM/PM指示项（如果存在）。");
+			FORMAT_DIC.Add("T", "长时间。");
+			FORMAT_DIC.Add("u", "用于显示通用时间的格式。");
+			FORMAT_DIC.Add("U", "使用通用时间的完整日期和时间（长日期和长时间）。");
+			FORMAT_DIC.Add("y", "年和月。");
+			FORMAT_DIC.Add("%y", "不包含纪元的年份，如果不包含纪元的年份小于10，则显示不具有前导零的年份。" + Desc("y"));
+			FORMAT_DIC.Add("yy", "不包含纪元的年份，如果不包含纪元的年份小于10，则显示具有前导零的年份。");
+			FORMAT_DIC.Add("yyyy", "包括纪元的四位数的年份。");
+			FORMAT_DIC.Add("Y", "年和月。");
+			FORMAT_DIC.Add("%z", "时区偏移量（“+”或“-”后面仅跟小时）。\n一位数的小时数没有前导零。例如，太平洋标准时间是“-8”。");
+			FORMAT_DIC.Add("zz", "时区偏移量（“+”或“-”后面仅跟小时）。\n一位数的小时数有前导零。例如，太平洋标准时间是“-08”。");
+			FORMAT_DIC.Add("zzz", "完整时区偏移量（“+”或“-”后面跟有小时和分钟）。\n一位数的小时数和分钟数有前导零。例如，太平洋标准时间是“-08:00”。");
+			FORMAT_DIC.Add("额外说明", "额外说明。");
+		}
+
+		/// <summary>
+		/// 日期时间格式字典。
+		/// </summary>
+		private static readonly Dictionary<string, string> FORMAT_DIC = new Dictionary<string, string>();
+
+		private static string Desc(string cf)
+		{
+			return string.Format("{0}(区别于格式{1}，当{2}不是单独出现时可以省略%)。", 
+				Environment.NewLine, cf, cf);
+		}
+
+		private void cmb_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			string format = (string)this.cmb.SelectedItem;
+			this.txt.Text = format;
+			UpdateLabel(format, this.ckb.Checked);
+			this.toolTip.SetToolTip(txt, FORMAT_DIC[format]);
+		}
+
+		private void ckb_CheckedChanged(object sender, EventArgs e)
+		{
+			int index = this.cmb.SelectedIndex;
+			if (index != -1)
+			{
+				UpdateLabel(this.cmb.Items[index].ToString(), this.ckb.Checked);
+			}
+		}
+
+		private void ckbEnable_CheckedChanged(object sender, EventArgs e)
+		{
+			this.txt.ReadOnly = !this.ckbEnable.Checked;
+			this.txt.BackColor = this.txt.ReadOnly ? Color.Wheat : Color.White;
+			this.toolTip.Active = this.txt.ReadOnly;
+			if (this.txt.ReadOnly && this.cmb.SelectedItem != null)
+			{
+				this.txt.Text = this.cmb.SelectedItem.ToString();
+				UpdateLabel(this.cmb.SelectedItem.ToString(), this.ckb.Checked);
+			}
+		}
+
+		private void UpdateLabel(string format, bool local)
+		{
+			if (format.Equals("额外说明"))
+			{
+				this.lbl.Text = ":    默认时间分隔符；\n" + 
+								"/   默认日期分隔符；\n" +
+								@"\c 其中c是任意字符。照原义显示字符。若要显示字符'\'，请使用“\\”。";
+				return;
+			}
+			var info = local ? DateTimeFormatInfo.CurrentInfo : DateTimeFormatInfo.InvariantInfo;
+			try { this.lbl.Text = DateTime.Now.ToString(format, info); }
+			catch (Exception) { this.lbl.Text = "Format Exception"; }
+		}
+
+		private void txt_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			if (e.KeyChar == (char)Keys.Enter)
+			{
+				UpdateLabel(this.txt.Text, this.ckb.Checked);
+				e.Handled = true;
+			}
+		}
+	}
+}
+/*
+ 参数format格式详细用法
+　格式字符 关联属性/说明
+　d ShortDatePattern
+　D LongDatePattern
+　f 完整日期和时间（长日期和短时间）
+　F FullDateTimePattern（长日期和长时间）
+　g 常规（短日期和短时间）
+　G 常规（短日期和长时间）
+　m、M MonthDayPattern
+　r、R RFC1123Pattern
+　s 使用当地时间的 SortableDateTimePattern（基于ISO8601）
+　t ShortTimePattern
+　T LongTimePattern
+　u UniversalSortableDateTimePattern 用于显示通用时间的格式
+　U 使用通用时间的完整日期和时间（长日期和长时间）
+　y、Y YearMonthPattern
+　C# 日期格式转换
+　下表列出了可被合并以构造自定义模式的模式。这些模式是区分大小写的；例如，识别“MM”，但不识别“mm”。如果自定义模式包含空白字符或用单引号括起来的字符，则输出字符串页也将包含这些字符。未定义为格式模式的一部分或未定义为格式字符的字符按其原义复制。
+　
+　格式模式 说明
+　d 月中的某一天。一位数的日期没有前导零。
+　dd 月中的某一天。一位数的日期有一个前导零。
+　ddd 周中某天的缩写名称，在 AbbreviatedDayNames 中定义。
+　dddd 周中某天的完整名称，在 DayNames 中定义。
+　M 月份数字。一位数的月份没有前导零。
+　MM 月份数字。一位数的月份有一个前导零。
+　MMM 月份的缩写名称，在 AbbreviatedMonthNames 中定义。
+　MMMM 月份的完整名称，在 MonthNames 中定义。
+　y 不包含纪元的年份。如果不包含纪元的年份小于 10，则显示不具有前导零的年份。
+　yy 不包含纪元的年份。如果不包含纪元的年份小于 10，则显示具有前导零的年份。
+　yyyy 包括纪元的四位数的年份。
+　gg 时期或纪元。如果要设置格式的日期不具有关联的时期或纪元字符串，则忽略该模式。
+　h 12 小时制的小时。一位数的小时数没有前导零。
+　hh 12 小时制的小时。一位数的小时数有前导零。
+　H 24 小时制的小时。一位数的小时数没有前导零。
+　HH 24 小时制的小时。一位数的小时数有前导零。
+　m 分钟。一位数的分钟数没有前导零。
+　mm 分钟。一位数的分钟数有一个前导零。
+　s 秒。一位数的秒数没有前导零。
+　ss 秒。一位数的秒数有一个前导零。
+　f 毫秒的小数精度为一位。其余数字被截断。
+　ff 毫秒的小数精度为两位。其余数字被截断。
+　fff 毫秒的小数精度为三位。其余数字被截断。
+　ffff 毫秒的小数精度为四位。其余数字被截断。
+　fffff 毫秒的小数精度为五位。其余数字被截断。
+　ffffff 毫秒的小数精度为六位。其余数字被截断。
+　fffffff 毫秒的小数精度为七位。其余数字被截断。
+　t 在 AMDesignator 或 PMDesignator 中定义的 AM/PM 指示项的第一个字符（如果存在）。
+　tt 在 AMDesignator 或 PMDesignator 中定义的 AM/PM 指示项（如果存在）。
+　z 时区偏移量（“+”或“-”后面仅跟小时）。一位数的小时数没有前导零。例如，太平洋标准时间是“-8”。
+　zz 时区偏移量（“+”或“-”后面仅跟小时）。一位数的小时数有前导零。例如，太平洋标准时间是“-08”。
+　zzz 完整时区偏移量（“+”或“-”后面跟有小时和分钟）。一位数的小时数和分钟数有前导零。例如，太平洋标准时间是“-08:00”。
+　: 在 TimeSeparator 中定义的默认时间分隔符。
+　/ 在 DateSeparator 中定义的默认日期分隔符。
+　% c 其中 c 是格式模式（如果单独使用）。如果格式模式与原义字符或其他格式模式合并，则可以省略“%”字符。
+　\ c 其中 c 是任意字符。照原义显示字符。若要显示反斜杠字符，请使用“\\”。
+ 
+ */
